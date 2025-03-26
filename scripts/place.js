9a2a23cf6668826dea9800faddcf252431e226c0
Isis <sewlanguedou@gmail.com>
2025-03-26 18:15:22 +0100

    yeah fifth attempt

--- scripts/place.js
+++ scripts/place.js
@@ -2,7 +2,7 @@ try {
     // Dynamically add the current year to the footer
     const currentYear = new Date().getFullYear();
     document.getElementById("currentyear").textContent = currentYear;
-
+    
     // Dynamically add the last modified date to the footer
     const lastModifiedDate = document.lastModified;
     document.getElementById("lastModified").textContent = `Last modified: ${lastModifiedDate}`;
